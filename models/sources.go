cbf2831dc4290a14f09b2bbf4b7319f679471275
constient-altrova <null>
2023-06-23 06:08:39 +0000

    🔄 created local 'models/' from remote 'models/'

---
+++ models/sources.go
@@ -0,0 +1,27 @@
+package models
+
+import "time"
+
+type Source struct {
+	ID          string     `json:"id"`
+	ProfileID   string     `json:"profileId"`
+	TeamID      string     `json:"teamId"`
+	Name        string     `json:"name"`
+	SourceType  int        `json:"sourceType"`
+	SourceToken string     `json:"sourceToken"`
+	Platform    string     `json:"platform"`
+	CreatedAt   time.Time  `json:"CreatedAt"`
+	UpdatedAt   time.Time  `json:"UpdatedAt"`
+	DeletedAt   *time.Time `json:"DeletedAt"` // Use a pointer to handle null values
+}
+
+type SourceResponse struct {
+	IsSuccessful bool     `json:"isSuccessful"`
+	Data         []Source `json:"data,omitempty"`
+	Message      []string `json:"message,omitempty"`
+}
+
+type SourceCreate struct {
+	Name       string `json:"name"`
+	SourceType int    `json:"sourceType"`
+}
