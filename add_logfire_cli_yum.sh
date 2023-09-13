190d5e3b85039c6940f012bdfbacb3805925b695
Tilak <mailtotilakkumar@gmail.com>
2023-09-13 16:45:08 +0530

    added yum install script

--- add_logfire_cli_yum.sh
+++ add_logfire_cli_yum.sh
@@ -1,12 +1,5 @@
 #!/bin/bash
 
-# Check if sudo is available
-if command -v sudo &> /dev/null; then
-    SUDO=sudo
-else
-    SUDO=""
-fi
-
 # Define the repository details
 REPO_NAME="logfire-cli"
 REPO_URL="https://logfire-sh.github.io/cli/yum-repo/"
@@ -14,7 +7,7 @@ GPG_KEY_URL="https://logfire-sh.github.io/cli/KEY.gpg"
 
 # Create the .repo file
 echo "Creating $REPO_NAME.repo in /etc/yum.repos.d/..."
-$SUDO tee /etc/yum.repos.d/$REPO_NAME.repo <<EOL
+tee /etc/yum.repos.d/$REPO_NAME.repo <<EOL
 [$REPO_NAME]
 name=Logfire CLI Repository
 baseurl=$REPO_URL
@@ -25,10 +18,10 @@ EOL
 
 # Import the GPG key
 echo "Importing GPG key..."
-$SUDO rpm --import $GPG_KEY_URL
+rpm --import $GPG_KEY_URL
 
 # Refresh the YUM cache
 echo "Refreshing YUM cache..."
-$SUDO yum makecache
+yum makecache
 
 echo "Logfire CLI Repository has been added and YUM cache has been refreshed."
